748dee886d72b5aaac67621cf5dcc2e73377e369
briank <briank@BRIANK>
2017-07-20 12:07:09 -0500

    Added 2 tabs- Get List of Users & Load List of Users to DB Also added command line options to execute the Load List of Users to DB via command line, on schedule.

--- SAP BO Admin Tools/Tools.vb
+++ SAP BO Admin Tools/Tools.vb
@@ -1,4 +1,6 @@
 ﻿Imports CrystalDecisions.Enterprise
+Imports System.Data.SqlClient
+Imports CrystalDecisions.Enterprise.Desktop
 
 Public Class Tools
 
@@ -13,25 +15,54 @@ Public Class Tools
     Private boEnterpriseSession As EnterpriseSession
     Private boInfoStore As InfoStore
     Private boSessionMgr As SessionMgr
+    Private boAlias As UserAliases
+    Private conSQLConn As SqlConnection
+    Private cmdSQLCmd As SqlCommand
 
-    Private Sub NewSession()
+    Private cmdAction As String = ""
+    Private cmdCMSServer As String = ""
+    Private cmdCMSUser As String = ""
+    Private cmdCMSUserPassword As String = ""
+    Private cmdCMSAuthentication As String = ""
+    Private cmdTargetServer As String = ""
+    Private cmdTargetDB As String = ""
+
+    Private Sub NewBOSession()
 
         Dim mgr As New SessionMgr
-        Dim userName As String = Me.txtCMSUserName.Text.ToString
-        Dim password As String = Me.txtCMSUserPassword.Text.ToString
-        Dim cMSName As String = Me.cboCMSServer.SelectedItem.ToString
-        Dim authentication As String = Me.cboCMSAuthentication.SelectedItem.ToString
-        Me.boEnterpriseSession = mgr.Logon(userName, password, cMSName, authentication)
+        Dim strUserName As String = ""
+        Dim strCMSName As String = ""
+        Dim strPassword As String = ""
+        Dim strAuthentication As String = ""
+
+        If cmdCMSUser.Length > 0 Then
+            strUserName = cmdCMSUser
+            strCMSName = cmdCMSServer
+            strPassword = cmdCMSUserPassword
+            strAuthentication = cmdCMSAuthentication
+        Else
+            strUserName = Me.txtCMSUserName.Text.ToString()
+            strCMSName = Me.cboCMSServer.SelectedItem.ToString()
+            strPassword = Me.txtCMSUserPassword.Text.ToString()
+            strAuthentication = Me.cboCMSAuthentication.SelectedItem.ToString()
+        End If
+
+        Me.rtbOutput.AppendText(ChrW(13) & ChrW(10))
+        Me.rtbOutput.AppendText("************************************************************************" & ChrW(13) & ChrW(10))
+        Me.rtbOutput.AppendText("Opening session to SAP BO." & ChrW(13) & ChrW(10))
+        Me.rtbOutput.AppendText(ChrW(13) & ChrW(10))
+        Me.rtbOutput.AppendText(ChrW(13) & ChrW(10))
+        Me.boEnterpriseSession = mgr.Logon(strUserName, strPassword, strCMSName, strAuthentication)
         Me.boInfoStore = New InfoStore(Me.boEnterpriseSession.GetService("InfoStore"))
 
     End Sub
 
-    Private Sub LogoffSession()
+    Private Sub LogoffBOSession()
 
         Me.boEnterpriseSession.Logoff()
         Me.boEnterpriseSession.Dispose()
         Me.rtbOutput.AppendText(ChrW(13) & ChrW(10))
-        Me.rtbOutput.AppendText("Closing session." & ChrW(13) & ChrW(10))
+        Me.rtbOutput.AppendText("Closing session to SAP BO." & ChrW(13) & ChrW(10))
         Me.rtbOutput.AppendText("************************************************************************" & ChrW(13) & ChrW(10))
         Me.rtbOutput.AppendText(ChrW(13) & ChrW(10))
         Me.rtbOutput.AppendText(ChrW(13) & ChrW(10))
@@ -57,13 +88,9 @@ Public Class Tools
 
     End Function
 
-
-
-
-
     Private Sub btnRepalceOwnerOnAllObjects_Click(sender As Object, e As EventArgs) Handles btnRepalceOwnerOnAllObjects.Click
 
-        Me.NewSession()
+        Me.NewBOSession()
 
         Dim intOwnerIdOld As Integer = Me.GetIdForUser(Me.txtReplaceOwnerOnAllObjectsOwnerNameOld.Text)
         Dim intOwnerIdNew As Integer = Me.GetIdForUser(Me.txtReplaceOwnerOnAllObjectsOwnerNameNew.Text)
@@ -90,13 +117,14 @@ Public Class Tools
             Me.boInfoStore.Commit(infoObjects)
         End If
 
-        Me.LogoffSession()
+        Me.LogoffBOSession()
 
     End Sub
 
     Private Sub btnReplaceOwnerOnSingeDoc_Click(sender As Object, e As EventArgs) Handles btnReplaceOwnerOnSingeDoc.Click
+
         Dim intDocId As Integer
-        Me.NewSession()
+        Me.NewBOSession()
 
         If Not Integer.TryParse(Me.txtReplaceOwnerOnSingleDocDocId.Text, intDocId) Then
             intDocId = -1
@@ -112,11 +140,11 @@ Public Class Tools
                     Try
                         enumerator = infoObjects.GetEnumerator
                         Do While enumerator.MoveNext
-                            Dim current As InfoObject = DirectCast(enumerator.Current, InfoObject)
-                            Dim str2 As String = current.Properties.Item("SI_NAME").Value.ToString
-                            Dim str3 As String = current.Properties.Item("SI_KIND").Value.ToString
-                            current.Properties.Item("SI_OWNERID").Value = intOwnerIdNew
-                            Dim textArray1 As String() = New String() {"Object updated: (", str3, ") ", str2, ChrW(13) & ChrW(10)}
+                            Dim objCurrentObject As InfoObject = DirectCast(enumerator.Current, InfoObject)
+                            Dim strObjectName As String = objCurrentObject.Properties.Item("SI_NAME").Value.ToString
+                            Dim strObjectKind As String = objCurrentObject.Properties.Item("SI_KIND").Value.ToString
+                            objCurrentObject.Properties.Item("SI_OWNERID").Value = intOwnerIdNew
+                            Dim textArray1 As String() = New String() {"Object updated: (", strObjectKind, ") ", strObjectName, ChrW(13) & ChrW(10)}
                             Me.rtbOutput.AppendText(String.Concat(textArray1))
                         Loop
                     Finally
@@ -132,7 +160,7 @@ Public Class Tools
         Else
             Me.rtbOutput.AppendText("Unable to parse object id" & ChrW(13) & ChrW(10))
         End If
-        Me.LogoffSession()
+        Me.LogoffBOSession()
 
     End Sub
 
@@ -140,7 +168,8 @@ Public Class Tools
 
         Dim strUserOwnerName As String = Me.txtListObjectsByOwnerOwnerName.Text.ToString
         Me.rtbOutput.AppendText("Begin User ID lookup" & ChrW(13) & ChrW(10))
-        Me.NewSession()
+
+        Me.NewBOSession()
 
         Dim intOwnerId As Integer = Me.GetIdForUser(strUserOwnerName)
         If (intOwnerId > 0) Then
@@ -170,16 +199,284 @@ Public Class Tools
                 End If
             End Try
         End If
-        Me.LogoffSession()
+
+        Me.LogoffBOSession()
 
     End Sub
 
     Private Sub Tools_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        '  Invoke this sample with an arbitrary set of command line arguments.
+        Dim arguments As String() = Environment.GetCommandLineArgs()
+
+        If arguments.Length > 1 Then
+            cmdAction = arguments(0)
+            cmdCMSServer = arguments(1)
+            cmdCMSUser = arguments(2)
+            cmdCMSUserPassword = arguments(3)
+            cmdCMSAuthentication = arguments(4)
+            cmdTargetServer = arguments(5)
+            cmdTargetDB = arguments(6)
+            MsgBox("GetCommandLineArgs: {0}", String.Join(", ", arguments))
+        End If
+
+        If cmdAction = "LoadUsersToDB" Then
+            GetBOUserList(False, True, cmdTargetDB, cmdTargetServer)
+        End If
+
         If cboCMSServer.Items.Count > 0 Then
             cboCMSServer.SelectedIndex = 0    ' The first item has index 0 '
         End If
         If cboCMSAuthentication.Items.Count > 0 Then
             cboCMSAuthentication.SelectedIndex = 0    ' The first item has index 0 '
         End If
+
     End Sub
+
+    Private Sub btnGetListOfUsers_Click(sender As Object, e As EventArgs) Handles btnGetListOfUsers.Click
+
+        Me.GetBOUserList(True, False)
+
+    End Sub
+
+    Private Sub btnLoadListOfUsersToDB_Click(sender As Object, e As EventArgs) Handles btnLoadListOfUsersToDB.Click
+
+        GetBOUserList(False, True, Me.txtLoadListOfUsersToDBDatabaseName.Text.ToString(), Me.txtLoadListOfUsersToDBSQLServerName.Text.ToString())
+
+    End Sub
+
+    Private Sub GetBOUserList(blnDisplay As Boolean, blnLoadDatabase As Boolean, Optional strDatabaseName As String = "", Optional strSQLServerName As String = "")
+
+        Me.NewBOSession()
+
+        Dim strQuery As String = ("Select SI_ID, SI_CUID, SI_NAME, SI_USERFULLNAME, SI_DESCRIPTION, SI_EMAIL_ADDRESS, SI_LASTLOGONTIME FROM CI_SYSTEMOBJECTS Where SI_KIND='User' ORDER BY SI_ID")
+        Dim objects As InfoObjects = Me.boInfoStore.Query(strQuery)
+
+        If (objects.Count > 0) Then
+            Dim enumerator As IEnumerator
+            Try
+                enumerator = objects.GetEnumerator
+                Do While enumerator.MoveNext
+                    Dim current As InfoObject = DirectCast(enumerator.Current, InfoObject)
+                    Dim strUserId As String = current.Properties.Item("SI_ID").Value.ToString()
+                    Dim strCUID As String = current.Properties.Item("SI_CUID").Value.ToString()
+                    Dim strName As String = current.Properties.Item("SI_NAME").Value.ToString()
+                    Dim strFullName As String = current.Properties.Item("SI_USERFULLNAME").Value.ToString()
+                    Dim strDescription As String = current.Properties.Item("SI_DESCRIPTION").Value.ToString()
+                    Dim strEmailAddress As String = current.Properties.Item("SI_EMAIL_ADDRESS").Value.ToString()
+
+                    'This property may not exist if the user has never logged on
+                    Dim strLastLogonTime As String
+                    Try
+                        strLastLogonTime = current.Properties.Item("SI_LASTLOGONTIME").Value.ToString()
+                    Catch ex As Exception
+                        strLastLogonTime = "01/01/1901"
+                    End Try
+
+                    Dim strDisabled = CheckIfUserIsDisabled(strUserId)
+
+                    If blnDisplay Then
+                        Dim arrResults As String() = New String() {"User found: (", strName, ") User ID: ", strUserId, "  Disabled: ", strDisabled, " Last Logon: ", strLastLogonTime, ChrW(13) & ChrW(10)}
+                        Me.rtbOutput.AppendText(String.Concat(arrResults))
+                    End If
+
+                    If blnLoadDatabase Then
+                        Me.SetSQLConnection(strDatabaseName, strSQLServerName)
+                        CreateUserTable()
+                        LoadUserToDatabase(strUserId, strCUID, strName, strFullName, strDescription, strEmailAddress, strLastLogonTime, strDisabled)
+                    End If
+
+                Loop
+            Finally
+                If TypeOf enumerator Is IDisposable Then
+                    TryCast(enumerator, IDisposable).Dispose()
+                End If
+            End Try
+        End If
+
+        Me.LogoffBOSession()
+
+    End Sub
+
+    Private Function CheckIfUserIsDisabled(strUserId As String) As String
+
+        Dim intBOAliasCount As Int16 = 0
+        Dim intTotalDisabled As Int16 = 0
+
+        Dim strQuery As String = ("Select SI_ALIASES FROM CI_SYSTEMOBJECTS WHERE SI_ID = " + strUserId)
+        Dim objects As InfoObjects = Me.boInfoStore.Query(strQuery)
+
+        'Loop through objects
+        If (objects.Count > 0) Then
+
+            Dim strDisabled As String = ""
+            Dim strAliasName As String = ""
+            Dim strAliasType As String = ""
+
+            Dim enumerator = objects.GetEnumerator
+            Try
+
+                Do While enumerator.MoveNext
+
+                    Dim objInfoObject As InfoObject = DirectCast(enumerator.Current, InfoObject)
+                    Dim objUser As User = DirectCast(objInfoObject, User)
+                    Dim objUserEnumerator = objUser.Aliases().GetEnumerator
+
+                    intBOAliasCount = objUser.Aliases.Count()
+
+                    Try
+
+                        Do While objUserEnumerator.MoveNext
+
+                            Dim boAlias As UserAlias = DirectCast(objUserEnumerator.Current, UserAlias)
+
+                            strDisabled = boAlias.Disabled.ToString()
+
+                            If strDisabled = "True" Then
+                                intTotalDisabled = intTotalDisabled + 1
+                            End If
+
+                            strAliasName = boAlias.Name()
+                            strAliasType = boAlias.Type()
+
+                        Loop
+
+                    Catch ex As Exception
+                    Finally
+                        If TypeOf objUserEnumerator Is IDisposable Then
+                            TryCast(objUserEnumerator, IDisposable).Dispose()
+                        End If
+
+                    End Try
+
+                Loop
+
+            Catch ex As Exception
+            Finally
+                If TypeOf enumerator Is IDisposable Then
+                    TryCast(enumerator, IDisposable).Dispose()
+                End If
+            End Try
+
+        End If
+
+        If intBOAliasCount = intTotalDisabled Then
+            CheckIfUserIsDisabled = "1"
+        Else
+            CheckIfUserIsDisabled = "0"
+        End If
+
+    End Function
+
+    Private Sub CreateUserTable()
+
+        ExecuteQuery("if Not exists " _
+                        & "(select 1 from sysobjects where name='DimSAPBOUser' and xtype='U') " _
+                        & "create table DimSAPBOUser (" _
+                        & "    SI_ID int not null primary key" _
+                        & "   , SI_CUID varchar(64) Not null" _
+                        & "   ,SI_NAME varchar(100) not null" _
+                        & "   ,SI_USERFULLNAME varchar(255) null" _
+                        & "   ,SI_DESCRIPTION varchar(max) null" _
+                        & "   ,SI_EMAIL_ADDRESS varchar(255) null" _
+                        & "   ,SI_LASTLOGONTIME datetime not null" _
+                        & "   ,Disabled bit not null Default 0" _
+                        & "   ,Deleted bit not null default 0" _
+                        & "   ,RecordInsertTimestamp datetime2(7) not null default sysdatetime()" _
+                        & "   ,RecordUpdateTimestamp datetime2(7) not null default sysdatetime()" _
+                        & " )")
+
+    End Sub
+
+    Private Sub LoadUserToDatabase(strUserID As String, strCUID As String, strName As String, strFullName As String, strDescription As String, strEmailAddress As String, strLastLogonTime As String, strDisabled As String)
+
+        Dim strQuery As String
+
+        strQuery = "merge " _
+                   & "      DimSAPBOUser As tgt" _
+                   & " Using " _
+                   & "     (" _
+                   & "         Select " _
+                   & "             " + strUserID + " As SI_ID" _
+                   & "             ,'" + strCUID + "' As SI_CUID " _
+                   & "             , '" + strName + "' As SI_NAME " _
+                   & "             , '" + strFullName + "' As SI_USERFULLNAME" _
+                   & "             , '" + strDescription + "' As SI_DESCRIPTION" _
+                   & "             , '" + strEmailAddress + "' As SI_EMAIL_ADDRESS" _
+                   & "             , '" + strLastLogonTime + "' As SI_LASTLOGONTIME" _
+                   & "             , " + strDisabled + " As Disabled" _
+                   & "      ) As src" _
+                   & " On tgt.SI_ID = src.SI_ID" _
+                   & " When matched And" _
+                   & " ( " _
+                   & "     IsNull(tgt.SI_NAME, '') <> IsNull(src.SI_NAME, '')" _
+                   & "  Or IsNull(tgt.SI_USERFULLNAME, '') <> IsNull(src.SI_USERFULLNAME, '')" _
+                   & "  Or IsNull(tgt.SI_DESCRIPTION, '') <> IsNull(src.SI_DESCRIPTION, '')" _
+                   & "  Or IsNull(tgt.SI_EMAIL_ADDRESS, '') <> IsNull(src.SI_EMAIL_ADDRESS, '')" _
+                   & "  Or IsNull(tgt.SI_LASTLOGONTIME, '') <> IsNull(src.SI_LASTLOGONTIME, '')" _
+                   & "  Or tgt.Disabled <> src.Disabled" _
+                   & " )" _
+                   & " then update " _
+                   & " set " _
+                   & "      tgt.SI_NAME               = src.SI_NAME " _
+                   & "     ,tgt.SI_USERFULLNAME       = src.SI_USERFULLNAME " _
+                   & "     ,tgt.SI_DESCRIPTION        = src.SI_DESCRIPTION " _
+                   & "     ,tgt.SI_EMAIL_ADDRESS      = src.SI_EMAIL_ADDRESS " _
+                   & "     ,tgt.SI_LASTLOGONTIME      = src.SI_LASTLOGONTIME " _
+                   & "     ,tgt.Disabled              = src.Disabled " _
+                   & "     ,tgt.RecordUpdateTimestamp = sysdatetime() " _
+                   & " when not matched by target then" _
+                   & " insert (" _
+                   & "             SI_ID" _
+                   & "            ,SI_CUID" _
+                   & "            ,SI_NAME" _
+                   & "            ,SI_USERFULLNAME" _
+                   & "            ,SI_DESCRIPTION" _
+                   & "            ,SI_EMAIL_ADDRESS" _
+                   & "            ,SI_LASTLOGONTIME" _
+                   & "            ,Disabled" _
+                   & "        )" _
+                   & " values" _
+                   & "        (" _
+                   & "             " + strUserID + "" _
+                   & "             , '" + strCUID + "'" _
+                   & "             , '" + strName + "'" _
+                   & "             , '" + strFullName + "'" _
+                   & "             , '" + strDescription + "'" _
+                   & "             , '" + strEmailAddress + "'" _
+                   & "             , '" + strLastLogonTime + "'" _
+                   & "             , " + strDisabled + "" _
+                   & "        )" _
+                   & " when not matched by source " _
+                   & "      and tgt.SI_ID = " + strUserID + " then " _
+                   & "        update set " _
+                   & "             tgt.Deleted = 1" _
+                   & "            ,tgt.Disabled = 1" _
+                   & " ;"
+
+        ExecuteQuery(strQuery)
+
+    End Sub
+
+
+    Private Sub SetSQLConnection(strDB As String, strServer As String)
+
+        'Create a Connection object.
+        conSQLConn = New SqlConnection("Initial Catalog=" + strDB + ";Data Source=" + strServer + ";Integrated Security=SSPI;")
+
+    End Sub
+
+    Private Sub ExecuteQuery(strQuery As String)
+
+        Dim cmdSQLCmd As New SqlCommand(strQuery, conSQLConn)
+
+        conSQLConn.Open()
+
+        cmdSQLCmd.ExecuteNonQuery()
+
+        conSQLConn.Close()
+
+    End Sub
+
+
 End Class
